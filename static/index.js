26f1f4fef40d6dbcccadec998f74ff16c0db7ff0
Aubrey Anderson <aubrey@bigsur.com>
2018-07-01 22:03:19 -0700

    Replace jQuery post with custom post

--- static/index.js
+++ static/index.js
@@ -5,12 +5,12 @@ $(() => {
 const handleClick = () => {
   var artist_name = $('#artist').val();
   var album_name = $('#album').val();
-  $.post('/', {
+  post('/', {
     artist: artist_name,
     album: album_name
   }).then(data => {
     if (data.id) {
-      return $.post('/', {id: data.id});
+      return post('/', {id: data.id});
     } else {
       throw "nothing found for " + artist_name + " " + album_name;
     }
@@ -21,4 +21,32 @@ const handleClick = () => {
       throw "nothing found for " + data.id;
     }
   }).catch(alert);
+};
+
+function post(host, data) {
+  return new Promise( function(resolve, reject) {
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", host, true);
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    
+    //Call a function when the state changes.
+    xhr.onreadystatechange = function() {
+      if(this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+        resolve(JSON.parse(this.responseText));
+      } else if (this.status != 200) {
+        reject("Request failed, response code: " + this.status);
+      }
+    }
+    xhr.send(URLEncode(data));
+  })
+}
+
+function URLEncode(data) {
+  var str = [];
+  for (var key in data) {
+    if (data.hasOwnProperty(key)) {
+      str.push(encodeURIComponent(key) + "=" + encodeURIComponent(data[key]));
+    }
+  }
+  return str.join('&');
 }
